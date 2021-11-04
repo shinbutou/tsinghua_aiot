8538bb0a98f333e53a0601b3f49c0d2751afeae7
Ian <chenghsi.yang@gmail.com>
2021-11-04 15:01:19 +0800

    finishing algorithms for data processing

--- task2/task2.m
+++ task2/task2.m
@@ -25,64 +25,120 @@ sexs = [];  % 1: male; 0: female
 bmis = [];
 children = [];
 smokers = [];  % 0: non-smoker; 1: smoker
-regions = [];  % 0: NE, 1: NW, 2: SE, 3: SW
+regions = [];  % 0: northeast, 1: northwest, 2: southeast, 3: southwest
 charges = [];
 
-heat = zeros(47, 39) % Figure 5
-
-% Figure 1
-male_charges = zeros(47, 'double');
-female_charges = zeros(47, 'double');
+male_charges = zeros(47, 1, 'double'); % Figure 1
+female_charges = zeros(47, 1, 'double'); % Figure 1
+male_smoker = []; % Figure 2
+female_smoker = []; % Figure 2
+male_overweight = []; % Figure 2
+female_overweight = []; % Figure 2
+northeast_charges = []; % Figure 3
+northwest_charges = []; % Figure 3
+southeast_charges = []; % Figure 3
+southwest_charges = []; % Figure 3
+scatter = zeros(); % Figure 4
+heat = zeros(47, 39); % Figure 5
 
 while ischar(tline)
     %disp(tline);
     line_data = split(tline, ",");
-
-    ages(end + 1) = str2double(line_data(1));
-    
-    if (string(line_data(2)) == "female")
+    ages(end + 1) = str2double(line_data(1));    
+    if string(line_data(2)) == "female"
         sexs(end + 1) = 0;
+        female_smoker(end + 1) = 0; % Figure 2
+        female_overweight (end + 1) = 0;
     else
         sexs(end + 1) = 1;
+        male_smoker(end+1) = 0; % Figure 2
+        male_overweight (end + 1) = 0;
     end
 
     bmis(end + 1) = str2double(line_data(3));
+    if bmis(end) > 25 && string(line_data(2)) == "female"
+        female_overweight (end) = 1;
+    elseif bmis(end) >25 && string(line_data(2)) == "male"
+        male_overweight (end) = 1;
+    end
+
     children(end + 1) = str2double(line_data(4));
     
     if (string(line_data(5)) == "no")
         smokers(end + 1) = 0;
+        smoker_cur = 0; % Figure 4
     else
         smokers(end + 1) = 1;
-    end
+        if string(line_data(2)) == "female"
+            female_smoker(end) = 1;
+        else
+            male_smoker(end) = 1;
+        end
 
-    switch string(line_data(6))
-        case 'northeast'
-            regions(end + 1) = 0;
-        case 'northwest'
-            regions(end + 1) = 1;
-        case 'southwest'
-            regions(end + 1) = 2;
-        case 'southeast'
-            regions(end + 1) = 3;
+        smoker_cur = 1; % Figure 4
     end
 
     charges(end + 1) = str2double(line_data(7));
 
+    switch string(line_data(6))
+        case 'northeast'
+            regions(end + 1) = 0;
+            northeast_charges(end + 1) = charges(end); % Figure 3
+        case 'northwest'
+            regions(end + 1) = 1;
+            northwest_charges(end + 1) = charges(end); % Figure 3
+        case 'southwest'
+            regions(end + 1) = 2;
+            southeast_charges(end + 1) = charges(end); % Figure 3
+        case 'southeast'
+            regions(end + 1) = 3;
+            southwest_charges(end + 1) = charges(end); % Figure 3
+    end
+
     tline = fgetl(fid);
 
-    % Figure 5
+    % Illustration Common Variables
     age_index = int64(str2double(line_data(1)));
     bmi_index = int64(str2double(line_data(3)));
     charge_var = str2double(line_data(7));
+
+    % Figure 1
+    switch string(line_data(2))
+        case 'male'
+            charge_cur = male_charges(age_index-17);
+            if charge_cur == 0
+                male_charges(age_index-17) = charge_var;
+            elseif charge_var < charge_cur
+                male_charges(age_index-17) = charge_var;
+            end
+
+        case 'female'
+            charge_cur = female_charges(age_index-17);
+
+            if charge_cur == 0
+                female_charges(age_index-17) = charge_var;
+            elseif charge_var < charge_cur
+                female_charges(age_index-17) = charge_var;
+            end
+    end
+
+    % Figure 2
+
+    % Figure 3
+
+    % Figure 4    
+    if smoker_cur == 1
+        scatter(age_index - 17, bmi_index - 14) = 1;
+    end
+
+    % Figure 5
     charge_cur = heat(age_index - 17, bmi_index - 14);
     
     if charge_var > charge_cur
         heat(age_index - 17, bmi_index - 14) = charge_var;
-    else
-        disp(charge_var);
     end
-
 end
+
 fclose(fid);
 
 % Figure 1 Min Charge of Each Age
@@ -91,7 +147,7 @@ fclose(fid);
 
 % Figure 3 Regional Box chart
 
-% Figure 4 Scatter of Smoker, BMI, and Children 
+% Figure 4 Scatter of Smoker, BMI, and  
 
 % Figure 5 Heatmap of Age, BMI, and Charge
 
